5e2a7014d0008a1cd5f661ccdcda18aa60cd6498
kazumichi kuroda <kazumichi96@gmail.com>
2024-09-04 04:49:21 +0900

    ResrouceManagerを実装

---
+++ Assets/Scripts/Core/Util/Util.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using System;
+
+public static class Util
+{
+  public static void ForEach<T>(T[] array, Action<T, int> func)
+  {
+    int count = array.Length;
+
+    for (int i = 0; i < count; ++i) {
+      func(array[i], i);
+    }
+  }
+
+  public static void ForEach<T>(T[] array, Func<T, int, bool> func)
+  {
+    int count = array.Length;
+    for (int i = 0; i < count; ++i) {
+      bool completed = func(array[i], i);
+      if (completed) break;
+    }
+  }
+
+  public static void ForEach<T1, T2>(Dictionary<T1, T2> dic, Action<T1, T2> func)
+  {
+    foreach (KeyValuePair<T1, T2> data in dic) {
+      func(data.Key, data.Value);
+    }
+  }
+
+  public static void ForEach<T1, T2>(Dictionary<T1, T2> dic, Func<T1, T2, bool> func)
+  {
+    foreach (KeyValuePair<T1, T2> data in dic) {
+      bool completed = func(data.Key, data.Value);
+      if (completed) break;
+    }
+  }
+
+  /// <summary>
+  /// リストの中からランダムに１つ取得する
+  /// </summary>
+  public static T GetRandom<T>(List<T> list)
+  {
+    return list[UnityEngine.Random.Range(0, list.Count)];
+  }
+
+  /// <summary>
+  /// 配列の指定したindexの要素を取得を試みる
+  /// </summary>
+  public static T TryGet<T>(T[] array, int index, T def = default)
+  {
+    if (0 <= index && index < array.Length) {
+      return array[index];
+    }
+
+    return default;
+  }
+
+  /// <summary>
+  /// List型のTryGet
+  /// </summary>
+  public static T TryGet<T>(List<T> list, int index) where T : class
+  {
+    if (0 <= index && index < list.Count) {
+      return list[index];
+    }
+
+    return null;
+  }
+
+  /// <summary>
+  /// 幸あれ！と祈ると指定した確率でtrueが返ってくる、幸がなければfalse
+  /// rateは0~1で指定
+  /// </summary>
+  public static bool GoodLuck(float rate)
+  {
+    return (UnityEngine.Random.Range(0, 1f) <= rate);
+  }
+}
---
+++ Assets/Scripts/Manager/ResourceManager.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.AddressableAssets;
+
+public class ResourceManager : SingletonMonoBehaviour<ResourceManager>
+{
+  /// <summary>
+  /// キャッシュリソース、参照カウンタとリソースの参照を保持するのみ
+  /// </summary>
+  private class CachedResource
+  {
+    /// <summary>
+    /// 参照カウンタ
+    /// </summary>
+    public uint Count = 0;
+
+    /// <summary>
+    /// リソース
+    /// </summary>
+    public UnityEngine.Object Resource = null;
+
+    /// <summary>
+    /// コンストラクタで参照カウンタを1に設定
+    /// </summary>
+    /// <param name="resource"></param>
+    public CachedResource(UnityEngine.Object resource)
+    {
+      this.Count = 1;
+      this.Resource = resource;
+    }
+  }
+
+  /// <summary>
+  /// キャッシュ済リソース、keyはリソースのアドレス
+  /// </summary>
+  private Dictionary<string, CachedResource> cache = new Dictionary<string, CachedResource>();
+
+  /// <summary>
+  /// ロードカウンター
+  /// </summary>
+  private int loadCounter = 0;
+
+  /// <summary>
+  /// ロード中ならばtrue
+  /// </summary>
+  public bool IsLoading {
+    get { return 0 < loadCounter; }
+  }
+
+  /// <summary>
+  /// リソースの非同期ロードを行う
+  /// </summary>
+  /// <typeparam name="T">ロードするリソースの種類</typeparam>
+  /// <param name="address">Addressable Asssetsで登録したAddress</param>
+  /// <param name="pre">リソースロード前に呼ばれるコールバック関数</param>
+  /// <param name="post">ロード完了したリソースを受け取る関数</param>
+  /// <param name="done">ロード完了時に呼ばれる関数</param>
+  public void Load<T>(string address, Action<T> post = null) where T : UnityEngine.Object
+  {
+    loadCounter++;
+    
+    Addressables.LoadAssetAsync<T>(address).Completed += op => {
+      // ロード完了時コールバックを実行
+      post?.Invoke(op.Result);
+      
+      loadCounter--;
+      if (op.Result == null) {
+        Logger.Error($"ResourceManager.Load:{address}がロードできませんでした。");
+        return;
+      }
+
+      // 未キャッシュであればキャッシュ、キャッシュ済であれば参照カウンタを更新
+      if (!this.cache.ContainsKey(address)) {
+        this.cache[address] = new CachedResource(op.Result);
+      }
+      else {
+        this.cache[address].Count++;
+      }
+    };
+  }
+
+  /// <summary>
+  /// キャッシュ済のリソースを取得、参照カウンタは変化しない
+  /// </summary>
+  public T GetCache<T>(string address) where T : UnityEngine.Object
+  {
+    if (!this.cache.ContainsKey(address)) {
+      Logger.Log($"ResourceManager.LoadSync: {address} is not found.");
+      return null;
+    }
+    else {
+      return this.cache[address].Resource as T;
+    }
+  }
+
+  /// <summary>
+  /// リソースの破棄、参照カウンタが0になるまでは実際の破棄は行われない
+  /// </summary>
+  /// <param name="address">破棄するリソースのアドレス</param>
+  public void Unload(string address)
+  {
+    // リソースが存在しなければ何もしない
+    if (!this.cache.ContainsKey(address)) {
+      Logger.Warn($"ResourceManager.Unload:{address}は読み込まれていないか、すでに破棄されています。");
+      return;
+    }
+
+    // キャッシュリソースを取得し、参照カウントを下げる
+    var cache = this.cache[address];
+    cache.Count--;
+
+    // 参照カウントが0であればリソースを解放する
+    if (cache.Count == 0) {
+      Addressables.Release(cache.Resource);
+      this.cache.Remove(address);
+    }
+  }
+
+#if _DEBUG
+  //-------------------------------------------------------------------------
+  // デバッグ
+  private string __InputText = "";
+
+  public override void OnDebug()
+  {
+    GUILayout.Label($"Loading Counter = {loadCounter}");
+
+    using (new GUILayout.VerticalScope(GUI.skin.box)) 
+    {
+      __InputText = GUILayout.TextField(__InputText);
+
+      using (new GUILayout.VerticalScope(GUI.skin.box)) 
+      {
+        Util.ForEach(this.cache, (key, value) => 
+        {
+          // 検索文字列が指定されている場合、該当しないリソースは表示しない
+          if (__InputText != "" && !key.Contains(__InputText)) {
+            return;
+          }
+
+          GUILayout.Label($"{key}:{value.Count}");
+        });
+      }
+    }
+  }
+#endif
+}
