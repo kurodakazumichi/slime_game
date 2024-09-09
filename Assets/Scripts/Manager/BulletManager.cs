b50bd0acd52dec8649c63d76dcc4c3d44afc6bb9
kazumichi kuroda <kazumichi96@gmail.com>
2024-09-09 12:52:19 +0900

    BulletFireInfoをファイル化

--- Assets/Scripts/Manager/BulletManager.cs
+++ Assets/Scripts/Manager/BulletManager.cs
@@ -3,18 +3,6 @@ using UnityEngine;
 using UnityEngine.Pool;
 
 /// <summary>
-/// 弾丸発射時の情報
-/// </summary>
-public class BulletFireInfo
-{
-  public Vector3 Position  = Vector3.zero;       // 位置
-  public Vector3 Direction = Vector3.zero;       // 方向
-  public IActor Target     = null;               // ターゲット
-  public ISkill Skill      = null;               // 弾丸を生成したスキル
-  public BulletOwner Owner = BulletOwner.Player; // 弾の所有者
-}
-
-/// <summary>
 /// 弾丸インターフェース
 /// </summary>
 public interface IBullet
