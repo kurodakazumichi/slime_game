5504cffa0a9763c39e3fb4542bae4b6b67e08d79
kazumichi kuroda <kazumichi96@gmail.com>
2024-09-09 12:44:50 +0900

    Attributeの定義位置を変更

--- Assets/Scripts/AttackStatus.cs
+++ Assets/Scripts/AttackStatus.cs
@@ -1,20 +1,3 @@
-﻿using UnityEngine;
-
-public enum Attribute
-{
-  Nil = 0,      // 設定なし
-  Non = 1 << 0, // 無
-  Fir = 1 << 1, // 火
-  Wat = 1 << 2, // 水
-  Thu = 1 << 3, // 雷
-  Ice = 1 << 4, // 氷
-  Tre = 1 << 5, // 木
-  Hol = 1 << 6, // 聖
-  Dar = 1 << 7, // 闇
-}
-
-
-
 public class AttackStatus
 {
 
--- Assets/Scripts/Define/App.cs
+++ Assets/Scripts/Define/App.cs
@@ -47,3 +47,19 @@ public enum Growth
   Slow,   // 遅い
   Fast,   // 速い
 }
+
+/// <summary>
+/// 属性
+/// </summary>
+public enum Attribute
+{
+  Nil = 0,      // 設定なし
+  Non = 1 << 0, // 無
+  Fir = 1 << 1, // 火
+  Wat = 1 << 2, // 水
+  Thu = 1 << 3, // 雷
+  Ice = 1 << 4, // 氷
+  Tre = 1 << 5, // 木
+  Hol = 1 << 6, // 聖
+  Dar = 1 << 7, // 闇
+}
