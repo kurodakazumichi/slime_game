bf3f6c24944d875789ef50e0d8d78c773c3072b7
kazumichi kuroda <kazumichi96@gmail.com>
2024-09-13 17:43:27 +0900

    FireBulletを利用できるように対応

--- Assets/Scripts/Manager/BulletManager.cs
+++ Assets/Scripts/Manager/BulletManager.cs
@@ -34,8 +34,10 @@ public class BulletManager : SingletonMonoBehaviour<BulletManager>
 
   public void Load()
   {
+
     ResourceManager.Instance.Load<GameObject>("Bullet/NormalBullet1/Object.prefab");
     ResourceManager.Instance.Load<GameObject>("Bullet/PiercingBullet1/Object.prefab");
+    ResourceManager.Instance.Load<GameObject>("Bullet/FireBullet1/Object.prefab");
   }
 
   private GameObject GetPrefab(SkillId id)
