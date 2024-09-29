ac6fd485da8d4bd32ff1339f2e830e8ea441f56e
kazumichi kuroda <kazumichi96@gmail.com>
2024-09-29 20:52:23 +0900

    ISkillEntityの定義場所を変更

--- Assets/Scripts/Master/Skill/SkillEntity.cs
+++ Assets/Scripts/Master/Skill/SkillEntity.cs
@@ -1,27 +1,4 @@
-﻿/// <summary>
-/// ReadOnlyのSkillEntityインターフェース
-/// </summary>
-public interface ISkillEntity
-{
-  SkillId Id { get; }
-
-  int MaxExp { get; }
-  float FirstRecastTime { get; }
-  float LastRecastTime { get; }
-  int FirstPower { get; }
-  int LastPower { get; }
-  int FirstPenetrableCount { get; }
-  int LastPenetrableCount { get; }
-  float SpeedGrowthRate { get; }
-  uint Attr { get; }
-  string Name { get; }
-  string Prefab { get; }
-  Growth GrowthType { get; }
-  float Impact { get; }
-  int IconNo { get; }
-  SkillAimingType Aiming { get; }
-
-}
+﻿
 
 /// <summary>
 /// Skill Entity
--- Assets/Scripts/Master/Skill/SkillMaster.cs
+++ Assets/Scripts/Master/Skill/SkillMaster.cs
@@ -1,6 +1,30 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// ReadOnlyのSkillEntityインターフェース
+/// </summary>
+public interface ISkillEntity
+{
+  SkillId Id { get; }
+
+  int MaxExp { get; }
+  float FirstRecastTime { get; }
+  float LastRecastTime { get; }
+  int FirstPower { get; }
+  int LastPower { get; }
+  int FirstPenetrableCount { get; }
+  int LastPenetrableCount { get; }
+  float SpeedGrowthRate { get; }
+  uint Attr { get; }
+  string Name { get; }
+  string Prefab { get; }
+  Growth GrowthType { get; }
+  float Impact { get; }
+  int IconNo { get; }
+  SkillAimingType Aiming { get; }
+
+}
 
 public static class SkillMaster
 {
